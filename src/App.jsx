04cc1cedcfb3450b1e35db5fd244ccfd62cbe526
egor.mikhalev <mamo4ka21196@gmail.com>
2024-10-24 14:25:09 +0300

    added key for list and condition to display list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
-import CardButton from './components/CardButton/CardButton';
-import JournalItem from './components/JournalItem/JournalItem';
+import { useState } from 'react';
 import LeftPanel from './layout/LeftPanel/LeftPanel';
 import Body from './layout/Body/Body';
 import Header from './components/Header/Header';
@@ -8,46 +7,44 @@ import JournalList from './components/JournalList/JournalList';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalForm from './components/JournalForm/JournalForm';
 
+const INITIAL_DATA = [
+	{
+		id: 1,
+		title: 'Подготовка к обновлению курсов',
+		text: 'Горные походы открывают удивительные природные ландшафт',
+		date: new Date()
+	},
+	{
+		id: 2,
+		title: 'Поход в годы',
+		text: 'Думал, что очень много времени',
+		date: new Date()
+	}
+];
 function App() {
-	const title = 'asda';
-	const date = new Date();
-	const text = 'asdawdasdsadasdasdasdasdasdasdasdasdas';
-	const data = [
-		{
-			title: title,
-			date: date,
-			text: text
-		},
-		{
-			title: title,
-			date: date,
-			text: text
-		}
-	];
+	const [items, setItems] = useState(INITIAL_DATA);
+
+	const addItem = (item) => {
+		setItems((oldItems) => [
+			...oldItems,
+			{
+				text: item.text,
+				title: item.title,
+				date: new Date(item.date),
+				id: Math.max(...oldItems.map((i) => i.id)) + 1
+			}
+		]);
+	};
+
 	return (
 		<div className="app">
 			<LeftPanel>
 				<Header />
 				<JournalAddButton />
-				<JournalList>
-					<CardButton>
-						<JournalItem
-							title={data[0].title}
-							date={data[0].date}
-							text={data[0].text}
-						/>
-					</CardButton>
-					<CardButton>
-						<JournalItem
-							title={data[1].title}
-							date={data[1].date}
-							text={data[1].text}
-						/>
-					</CardButton>
-				</JournalList>
+				<JournalList items={items} />
 			</LeftPanel>
 			<Body>
-				<JournalForm />
+				<JournalForm onSubmit={addItem} />
 			</Body>
 		</div>
 	);
