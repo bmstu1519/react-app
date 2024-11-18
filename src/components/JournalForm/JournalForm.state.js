045bc9f32d31ed8799b2e49b51deb2f850901893
egor.mikhalev <mamo4ka21196@gmail.com>
2024-11-18 08:26:42 +0300

    added state

---
+++ src/components/JournalForm/JournalForm.state.js
@@ -0,0 +1,13 @@
+export const INITIAL_STATE = {
+	isValid: {
+		post: true,
+		title: true,
+		date: true
+	},
+	values: {
+		post: undefined,
+		title: undefined,
+		date: undefined
+	},
+	isFormReadyToSubmit: false
+};
