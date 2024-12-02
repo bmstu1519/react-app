a0d6905f8fbaef258dab5a2b5cb9f90e0878c739
egor.mikhalev <mamo4ka21196@gmail.com>
2024-12-02 20:50:14 +0300

    clean aup

--- src/components/CardButton/CardButton.jsx
+++ src/components/CardButton/CardButton.jsx
@@ -1,4 +1,5 @@
 import './CardButton.css';
+
 function CardButton({ children, className }) {
 	const cl = 'card-button' + (className ? ' ' + className : '');
 	return <button className={cl}>{children}</button>;
